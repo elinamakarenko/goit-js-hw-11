9cd1b70c911ed3f457ed7a701b2a91d75388a2fd
elinamakarenko <68436291+elinamakarenko@users.noreply.github.com>
2022-03-29 22:39:09 +0300

    first-commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// import _ from 'lodash';
+const KEY = "26380962-1c7476655555a971bf37ffb07";
